175607c372a36963ca199406ffd31421ec3664c3
Ekaterina Grishina <grishinaekateina@gmail.com>
2024-10-13 15:26:28 +0300

    added configs

--- script_calc_errs.py
+++ script_calc_errs.py
@@ -1,7 +1,11 @@
+import hydra
 from src.metrics.utils import calc_cer, calc_wer
 
 
-def calc_cer_wer(target_path, pred_path):
+@hydra.main(version_base=None, config_path="src/configs", config_name="calc_cer_wer")
+def main(config):
+    target_path = config.target_path
+    pred_path = config.pred_path
     with open(target_path, 'r') as f:
         target = f.read()
     with open(pred_path, 'r') as f:
@@ -9,3 +13,7 @@ def calc_cer_wer(target_path, pred_path):
     wer = calc_wer(target, pred)
     cer = calc_cer(target, pred)
     return cer, wer
+
+
+if __name__ == "__main__":
+    main()
